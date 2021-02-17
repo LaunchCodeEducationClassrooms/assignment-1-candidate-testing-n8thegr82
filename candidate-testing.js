39520c8052e5129c60b44203027116962bb5b600
Nathan <ngrowcock@gmail.com>
2021-02-17 02:01:41 +0000

    test 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,43 +3,65 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+  let question = "Who was the first American woman in space? "; 
+  
+  let candidateAnswer = '';
+
+  let correctAnswer = 'Sally Ride';
+  
+let questions = ['Who was the first American woman in space? ', 'True or false: 5000 meters = 5 kilometers. ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the International Space Station (ISS)? '];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question ('What is your Name?');
 }
 
-function askQuestion() {
+
+function askQuestion(question) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  candidateAnswer = input.question (question);
+} 
 
 
-}
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-  let grade;
+  
   
 
+
+  let grade = 0;
+  let score = 0;
+  for (let i = 0; i < candidateAnswers.length; i++)
+  {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase())
+    
+    { 
+score++;
+
+    }
+  
+  }
+grade = (score/correctAnswers.length) *100;
   return grade;
+
+
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
+  console.log('Hello, ' + candidateName + '!');
+
+
+  askQuestion(question);
   gradeQuiz(this.candidateAnswers);
 }
 
